edeb1c01cc4b4b6db4ed6b93ebe1f164cb66675b
GGhiaroni <gghiaronitiziano@gmail.com>
2024-12-24 01:28:38 -0300

    adicao de sons no botao e logica para mudar icone e texto do botao comecar

--- script.js
+++ script.js
@@ -3,8 +3,22 @@ const imagemBanner = document.querySelector('.app__image');
 const botoes = document.querySelectorAll('button:not(#start-pause)');
 const titulo = document.querySelector('.app__title');
 const inputMusica = document.querySelector('.toggle-checkbox');
+const botaoComecar = document.getElementById('start-pause');
+const imagemBotaoComecar = document.querySelector('.app__card-primary-butto-icon');
+const textoBotaoComecar = document.getElementById('texto-botao');
 const musica = new Audio('/sons/luna-rise-part-one.mp3');
+const somBotaoPlay = new Audio('/sons/play.wav');
+const somBotaoPause = new Audio('/sons/pause.mp3');
+const somTarefaTerminada = new Audio('sons/beep.mp3');
+
 musica.loop = true;
+somBotaoPlay.volume = 0.3;
+somBotaoPause.volume = 0.2;
+somTarefaTerminada.volume = 0.1;
+
+
+let tempoDecorrido = 5;
+let intervaloId = null;
 
 botoes.forEach(botao => {
     botao.addEventListener('click', (e) => {
@@ -50,3 +64,51 @@ function alterarContexto(contexto)
 inputMusica.addEventListener('change', () => {
     musica.paused ? musica.play() : musica.pause();
 });
+
+const contagemRegressiva = () => {
+    if (tempoDecorrido <= 0)
+    {
+        somTarefaTerminada.play();
+        zerar();
+        alert("Sessão finalizada!");
+        return;
+    }
+    tempoDecorrido -= 1;
+    console.log("Temporizador: " + tempoDecorrido);
+ };
+
+botaoComecar.addEventListener('click', () => {
+    iniciarOuPausar();
+    alterarIconeBotao();
+});
+
+function alterarIconeBotao() {
+    if (botaoComecar.classList.contains('ativo')) {
+        botaoComecar.classList.remove('ativo');
+        imagemBotaoComecar.setAttribute('src', '/imagens/play_arrow.png');
+        textoBotaoComecar.textContent = 'Iniciar';
+        somBotaoPause.play();
+    } else {
+        botaoComecar.classList.add('ativo');
+        imagemBotaoComecar.setAttribute('src', '/imagens/pause.png');
+        textoBotaoComecar.textContent = 'Pausar';
+        somBotaoPlay.play();
+    }
+}
+
+
+function iniciarOuPausar()
+{
+    if (intervaloId)
+    {
+        zerar();
+        return;
+    }
+    intervaloId = setInterval(contagemRegressiva, 1000);
+}
+
+function zerar()
+{
+    clearInterval(intervaloId);
+    intervaloId = null;
+}
