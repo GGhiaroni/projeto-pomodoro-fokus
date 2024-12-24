0f863d1290829d458491a4ca4db243bc88428527
GGhiaroni <gghiaronitiziano@gmail.com>
2024-12-24 19:38:07 -0300

    comecando a inserir dados no localstorage

--- script-crud.js
+++ script-crud.js
@@ -1,6 +1,18 @@
 const botaoAdicionarTarefa = document.querySelector('.app__button--add-task');
 const formAdicionarTarefa = document.querySelector('.app__form-add-task');
+const textArea = document.querySelector('.app__form-textarea');
+
+const tarefas = [];
 
 botaoAdicionarTarefa.addEventListener('click', () => {
     formAdicionarTarefa.classList.toggle('hidden');
+});
+
+formAdicionarTarefa.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const tarefa = {
+        descricao: textArea.value
+    };
+    tarefas.push(tarefa);
+    localStorage.setItem('tarefas', tarefas);
 });
